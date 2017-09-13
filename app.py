643c30c35ad305b136c22c8235af275dfa4b60e0
Ben Matheja <post@benmatheja.de>
2017-09-13 21:55:46 +0200

    restructured logging

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, request, jsonify
-import gzip, StringIO, json
-from logging.handlers import RotatingFileHandler
-import logging
 import datetime
+import gzip
+import json
+import logging
+import uuid
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask, request, jsonify, g
 
 app = Flask(__name__)
 
 
 @app.before_first_request
 def setup_logging():
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s",
+                              "%Y-%m-%d %H:%M:%S")
     handler = RotatingFileHandler('longview-backend.log', maxBytes=2000000, backupCount=1)
     handler.setLevel(logging.DEBUG)
     handler.setFormatter(formatter)
@@ -18,18 +22,24 @@ def setup_logging():
 
 @app.before_request
 def log_request_info():
-    app.logger.debug('Headers: %s', request.headers)
+    # app.logger.debug('Headers: %s', request.headers)
     # app.logger.debug('Body: %s', request.get_data())
+    g.begin_time = datetime.datetime.now()
+    if 'trace-id' not in request.headers:
+        g.event_id = uuid.uuid4().__str__()
+    else:
+        g.event_id = request.headers.get('trace-id')
+    if 'X-Real-IP' not in request.headers:
+        g.real_ip = request.remote_addr.__str__()
+    else:
+        g.real_ip = request.headers.get('X-Real-IP')
 
 
 @app.route('/longview', methods=['POST'])
 def handle_post():
-    begin_time = datetime.datetime.now()
-    # app.logger.debug(request.files)
-    # for filename, file in request.files.iteritems():
-    #   app.logger.debug(request.files[filename].name)
-    # app.logger.debug(request.files.get('data'))
-
+    if 'User-Agent' not in request.headers:
+        raise
+    
     file = request.files.get('data').stream
     uncompressed = gzip.GzipFile(fileobj=file, mode='rb')
     with uncompressed as fin:
@@ -38,26 +48,61 @@ def handle_post():
         data = json.loads(json_str)
 
     process_json(data)
-    return jsonify({'status': 'accepted'}), 200
+
+    log_info(g.begin_time, g.real_ip, 'accepted', g.event_id,
+             'Processing completed')
+
+    return jsonify({'status': "accepted",
+                    'trace-id': g.event_id}), 200
+
 
 @app.route('/longview', methods=['GET'])
 def handle_get():
     with open('payload.json') as data_file:
         data = json.load(data_file)
     process_json(data)
-    return jsonify({'status': 'accepted'}), 200
+    log_info(g.begin_time, g.real_ip, 'accepted', g.event_id,
+             'Processing completed')
+
+    return jsonify({'status': "accepted",
+                    'trace-id': g.event_id}), 200
+
+
+@app.errorhandler(500)
+def internal_error(exception):
+    return jsonify({'status': "internal error"}), 500
+
 
 def process_json(data):
     longterm = data["payload"][0]["LONGTERM"]
     instant = data["payload"][0]["INSTANT"]
-    #app.logger.debug(longterm)
-    #app.logger.debug(instant)
 
-    app.logger.debug(longterm["Network.Interface.eth0.tx_bytes"])
-    app.logger.debug(longterm["Network.Interface.eth0.rx_bytes"])
-    app.logger.debug(longterm["CPU.cpu0.system"])
-    app.logger.debug(longterm["CPU.cpu0.user"])
-    app.logger.debug(longterm["CPU.cpu0.wait"])
+    event_object = {}
+
+    event_object['host'] = instant['SysInfo.hostname']
+    event_object['timestamp'] = data["payload"][0]['timestamp']
+
+    # Omit Keys for Processes
+    for key, value in longterm.iteritems():
+        if 'Processes.' not in key:
+            event_object[key] = value
+
+    #app.logger.debug(json.dumps(event_object))
+
+
+def log_info(begin_time, request_ip, status, trace_id, message, service='handle_post'):
+    end = datetime.datetime.now()
+    processing_time = (end - begin_time).microseconds.__str__()
+    app.logger.info(
+        trace_id + ' - ' + request_ip + ' - ' + service + ' - ' + processing_time + ' - ' + status + ' - ' + message)
+
+
+def log_debug(begin_time, request_ip, status, trace_id, message, service='handle_post'):
+    end = datetime.datetime.now()
+    processing_time = (end - begin_time).microseconds.__str__()
+    app.logger.debug(
+        trace_id + ' - ' + request_ip + ' - ' + service + ' - ' + processing_time + ' - ' + status + ' - ' + message)
+
 
 if __name__ == '__main__':
     app.run(debug=False)
